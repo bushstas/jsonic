96cdedc228274e2244ee0eb205e6d9eeabe91d23
Bushmakin Stas <bushstas@mail.ru>
2017-03-30 17:49:46 +0500

    foreach parser

--- www/builder/builder-php-classes/builder.foreach.code.parser.php
+++ www/builder/builder-php-classes/builder.foreach.code.parser.php
@@ -3,6 +3,40 @@
 class ForeachCodeParser 
 {	
 	public static function parse($content) {
-		Printer::log($content);
+		$content = preg_replace('/^foreach\s*/', '', $content);
+		list($items, $rest) = self::select('as', $content);
+		list($rest, $while) = self::select('while', $rest);
+		list($rest, $limit) = self::select('limit', $rest);
+		list($key, $value) = self::select('=>', $rest);
+
+		if (empty($value)) {
+			$value = $key;
+			$key = null;
+		}
+		
+
+
+		self::parseItems($items);
+		if (!empty($key)) {
+			self::parseKey($key);
+		}
+		self::parseValue($value);
+		self::parseWhile($while);
+		self::parseLimit($limit);
+
+
+
+	}
+
+	private static function parseItems($items) {
+		 
+	}
+
+	private static function select($keyword, $content) {
+		$parts = explode($keyword, $content);
+		foreach ($parts as &$part) {
+			$part = trim($part);
+		}
+		return $parts;
 	}
 }
